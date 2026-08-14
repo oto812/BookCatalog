73ac1da12e0961758260320b412747a5b3c8b89a
Otar Abashidze <oabashidze2706@geoh.ge>
2026-08-14 16:29:40 +0400

    feat(domain): added Book entity and Genre enum

---
+++ BookCatalog.Domain/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using BookCatalog.Domain.Enums;
+
+
+namespace BookCatalog.Domain.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; } 
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+        public Genre Genre { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
