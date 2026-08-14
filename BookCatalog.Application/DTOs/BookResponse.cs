77527887d77dcf74097e8d90ba0da529cb8abcf2
Otar Abashidze <oabashidze2706@geoh.ge>
2026-08-14 17:35:57 +0400

    feat(application): added book request and response DTOs

---
+++ BookCatalog.Application/DTOs/BookResponse.cs
@@ -0,0 +1,12 @@
+﻿using BookCatalog.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookCatalog.Application.DTOs
+{
+    public record BookResponse(Guid Id,
+    string Title,
+    string Author,
+    Genre genre);
+}
---
+++ BookCatalog.Application/DTOs/CreateBookRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookCatalog.Application.DTOs
+{
+    public record CreateBookRequest(
+        string Title,
+        string Author,
+        int PublicationYear,
+        string Genre
+    );
+}
