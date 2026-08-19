3c52faeadfaa957d87106958c34e633789635a1d
Otar Abashidze <oabashidze2706@geoh.ge>
2026-08-19 19:38:49 +0400

    feat(api): add BooksController and register services in DI

---
+++ BookCatalog.Api/Controllers/BooksController.cs
@@ -0,0 +1,51 @@
+﻿using BookCatalog.Application.DTOs;
+using BookCatalog.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookCatalog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<BookResponse>> GetAllBooks()
+        {
+            var books = _bookService.GetAllBooks();
+
+            return Ok(books);
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<BookResponse> GetBookById(Guid id)
+        {
+            var response = _bookService.GetBookById(id);
+            if (response == null)
+            {
+                return NotFound();
+            }
+            return response;
+        }
+
+        [HttpPost]
+        public ActionResult<BookResponse> AddBook(CreateBookRequest createBookRequest)
+        {
+            var book = _bookService.AddBook(createBookRequest);
+            if (book == null) {
+                 return Conflict();
+            }
+
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+
+
+    }
+}
