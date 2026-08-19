11ea1d49837bb4b6e6d0dead075748c761800c2d
Otar Abashidze <oabashidze2706@geoh.ge>
2026-08-19 19:38:33 +0400

    feat(application): add book service layer

---
+++ BookCatalog.Application/Services/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using BookCatalog.Application.DTOs;
+using BookCatalog.Domain.Entities;
+
+
+namespace BookCatalog.Application.Services
+{
+    public interface IBookService
+    {
+        public BookResponse? AddBook(CreateBookRequest book);
+        public BookResponse? GetBookById(Guid id);
+        public IEnumerable<BookResponse> GetAllBooks();
+    }
+}
